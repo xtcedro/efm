850a271fd315b31b2f7a1a7b83c430970197a86c
xtcedro <pedro.dfedro@gmail.com>
2025-03-16 19:28:02 -0500

    update

--- public/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-import { loadHeader, loadFooter } from './load-components.js';
-import { setupNavigation } from './navigation.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-    loadHeader();
-    loadFooter();
-    setupNavigation();
-});

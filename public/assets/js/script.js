e24a37ac429c2891ed62fc569113ae53e23339c9
Pedro M. Dominguez <84252067+xtcedro@users.noreply.github.com>
2025-03-17 14:51:39 -0500

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,12 @@
 import { loadHeader, loadFooter } from './load-components.js';
 import { setupNavigation } from './navigation.js';
+import { submitAppointments } from './appointment-booker.js';
+import { fetchAppointments } from './public-appointments.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     loadHeader();
     loadFooter();
     setupNavigation();
+    submitAppointments();
+    fetchAppointments();
 });
